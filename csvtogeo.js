8175ff95256b4dd0281e38407a1a2e75d3a89e52
Laurence Carlucci <laurence@archlinux.localdomain>
2018-04-28 15:40:07 +0000

    qys infmration with [popups (properties):wq :wq :wq :wq i :wq

--- csvtogeo.js
+++ csvtogeo.js
@@ -18,7 +18,6 @@ function handle(fils){
 	//document.write(fils);
 	if(window.FileReader){
 		getText(fils[0]);
-		
 		/*getText(fils[0],function(){
 			//g = sec;
 			//$("#glob").html(g);
@@ -36,7 +35,6 @@ function handle(fils){
 	var infile = new FileReader();
 	//infile.readAsText("/home/laurence/Desktop/JJOH/a.txt");
 	infile.readAsText(fil);
-	
 	/*
 	infile.onload = function(event) {
                 var csvin = event.target.result;
@@ -64,39 +62,42 @@ function errorHandle(evnt){
 
 function ctog(csvin){ //csvin,geoout) {
 	var rows = csvin.split(/\r\n|\n/);
-	//var re = /"[^"]*"|[^,]+/;
-	//var re = /"[^"\\]*(?:\\.[^"\\]*)*"|[^,]+/;
-	//var re = /",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))"/;
-	var re = /,/;
-	//document.write("<br>");
 	var hfound = false;
-	var year;
-	var lat;
-	var lon;
-	var nam;
-	var crim;
-	var cols;
+	var year, lat, lon, nam, crim, cols0, cols1;
 	var ret = "";
 	for (var i= 0; i<rows.length; i++){
-		cols = rows[i].split(re); 
+		cols1 = rows[i].split(/,/);
+		/*cols0 = rows[i].split(/,\"/);
+		window.alert(cols0);
+		for(var j = 0; j < cols0.length; j++){
+			var temp = cols0[j].split(/\"/);
+			 cols1.push(temp[0]);
+			 if(temp[1]!==undefined && temp[1].length!=0){
+			 	temp = temp[1].substring(1).split(/,/);
+			 	for (var k = 0; k<temp.length; k++){
+        			cols1.push(temp[k]);
+        			//window.alert(temp[k]);
+        		}
+			 }
+		}*/
 		//document.write(cols.length+"<br>");
 		if (hfound){
-				year = cols[yearpos];
-				lat = randlat(); //cols[latpos];
-				lon =  randlon(); //cols[lonpos];
-				nam = cols[nampos];
-				crim = cols[crimpos];
+				year = cols1[yearpos];
+				lat = randlat(); //cols1[latpos];
+				lon =  randlon(); //cols1[lonpos];
+				nam = cols1[nampos];
+				crim = cols1[crimpos];
 				ret+= '{"type": "Feature","properties": {"year": '+'"'+year+'"'+', "name": '+'"'+nam+'"'+', "crime": '+'"'+crim+'"'+'},';
 				ret+= '"geometry": {"type": "Point", "coordinates": ['+lat+','+lon+']}}';
 				if (i!=rows.length-1)
 					ret+=',';
 			}
-			else if(cols.indexOf("Year ") != -1){
-					var yearpos = cols.indexOf("Year ");
-					var latpos = cols.indexOf("Latitude");
-					var lonpos = cols.indexOf("Longitude"); 
-					var nampos = cols.indexOf("Individuals (all who are named)");
-					var crimpos = cols.indexOf("Crime (in Spanish)") ;
+			else if(cols1.indexOf("Year ") != -1){
+					var yearpos = cols1.indexOf("Year ");
+					var latpos = cols1.indexOf("Latitude");
+					var lonpos = cols1.indexOf("Longitude"); 
+					var nampos = cols1.indexOf("Individuals (all who are named)");
+					var crimpos = cols1.indexOf("Crime (in Spanish)") ;
 					hfound = true;
 					ret += ('{"type": "FeatureCollection","features": [');
 			}
@@ -108,4 +109,48 @@ function ctog(csvin){ //csvin,geoout) {
 		//return ret;
 		//window.alert(ret);
 		//secret(ret);
+		/*
+		var a = '1888,"Luis, y dos Compas","por avijeo, y robo",olaolaola,orange,banana,"Precidiados, 3",hola,Conclusa';
+var b = a.split(/,\"/);
+var c = [];
+for (var i = 0; i<b.length; i++){
+  		var temp = b[i].split(/\"/);
+      c.push(temp[0]);
+     if(temp[1]!==undefined && temp[1].length!=0){
+      	//document.write(temp[1]); 
+      	temp = temp[1].substring(1).split(/,/);
+        for (var j = 0; j<temp.length; j++){
+        	c.push(temp[j]);
+        }
+       }
+  	}
+  for (var i = 0;i<c.length; i++){
+ 	document.write(c[i]+"<p>");
+}
+
+var year, lat, lon, nam, crim, cols0;
+var cols1=[];
+var csvin = '7, james, jolly jerry, 45, a,"banana, mango"\n77, jack';
+var rows = csvin.split(/\r\n|\n/);
+for (var i= 0; i<rows.length; i++){
+		cols0 = rows[i].split(/,\"/);
+		for(var j = 0; j < cols0.length; j++){
+			var temp = cols0[j].split(/\"/);
+			cols1.push(temp[0]);
+     
+			 if(temp[1]!==undefined && temp[1].length!=0){
+       console.log(cols1);
+       window.alert(temp.length);
+			 	temp = temp[1].substring(1).split(/,/);
+        //window.alert(temp.length);
+			 	for (var k = 0; k<temp.length; k++){
+        		//window.alert(temp[k]);
+        			//cols1.push(temp[k]);
+        			//window.alert(temp[k]);
+              console.log(temp[k]);
+        		}
+			 }
+		}
+    }
+		*/
 	}
